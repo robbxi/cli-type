3107aa7d52725669235f64414b1334ee19b61d6b
robbxi <rsmi315@lsu.edu>
2024-03-07 16:41:40 -0600

    Made test restartable and added a demo gif

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import random
 from blessed import Terminal
 import time
 import math
-#
+
+#Initialize list of words to be used in test
 words = [
     "the", "of", "to", "and", "a", "in", "is", "it", "you", "that", "he", "was",
     "for", "on", "are", "with", "as", "I", "his", "they", "be", "at", "one", "have",
@@ -86,31 +87,40 @@ words = [
     "liquid", "log", "meant", "quotient", "teeth", "shell", "neck"
 ]
 
+#Initialize terminal
 term = Terminal()
 
-#print(term.home + term.clear + term.move_y(term.height // 2))
-#print(term.black_on_darkkhaki(term.center('press any key to continue.')))
 
-#with term.cbreak(), term.hidden_cursor():
- #   inp = term.inkey()
-
-##print(term.move_down(2) + 'You pressed ' + term.bold(repr(inp)))
-#
-
+#Initialize variables
 testActive = True
 inp = ''
-x = 10
-y = 0
-FPS = 15
-testLength = 20
-testWords = ''
 
-for i in range(testLength):
-   testWords += words[random.randint(0,len(words)-1)] + ' ' if i != testLength-1 else words[random.randint(0,len(words)-1)]
+#Get test length from user
+input = input("Enter test length in number of words: ")
 
+#set default test length to 10
+testLength = 10
+
+#try to convert input to int, if not possible, default to 10
+try:
+    testLength = int(input)
+except:
+    print("Invalid input, defaulting to 10")
+
+
+#Function to create test from random words in list
+def createTest():
+    testWords = ''
+    for i in range(testLength):
+        testWords += words[random.randint(0,len(words)-1)] + ' ' if i != testLength-1 else words[random.randint(0,len(words)-1)]
+    return testWords
+
+#Create test
+testWords = createTest()
+
+#initialize test variables
 charCount = len(testWords)
 
-missed = 0
 
 correct = 0
 
@@ -118,11 +128,12 @@ testTime = 0
 
 width = term.width
 
+#user input string
 typed = ''
 
 
 
-
+#display original test words
 def draw():
     y = 5
     print(term.home  + term.clear)
@@ -136,6 +147,7 @@ def draw():
         line = ''
         y += 1
 
+#display typed words, correct characters in green, incorrect in red, next character in gold
 def drawTyped():
     i = 0
     y = 5
@@ -150,7 +162,7 @@ def drawTyped():
                 print(term.move_xy(10+lineLength, y) + term.red(typed[i]))
 
             if lineLength+1<width - 20:
-                print(term.move_xy(10+lineLength+1, y) + term.yellow(testWords[i+1]))
+                print(term.move_xy(10+lineLength+1, y) + term.gold(testWords[i+1]))
             else:
                 print(term.move_xy(10, y+1) + term.gold(testWords[i+1]))
 
@@ -161,53 +173,84 @@ def drawTyped():
 
 
 
+#draw original test
 draw()
 
+#initialize test start time
 start = 0
 
+#initialize first character flag
 first = True
-while True:
+
+#enter main loop
+while testActive:
 
     if len(typed) >= len(testWords):
             testActive = False
-            break
+            
    
     if testActive:
         drawTyped()
             
+        #get user input
         with term.cbreak(), term.hidden_cursor():
             inp = term.inkey()
 
-
-        if len(typed) < len(testWords) - 1:
+        #compare input to correct character
+        if len(typed) <= len(testWords):
             if inp == testWords[len(typed)]:
                 correct += 1
 
+        #if user presses Q, quit test and display results
         if inp == 'Q':
             testActive = False
+        #if user presses backspace, remove last character from typed and redraw
         elif inp.name == "KEY_BACKSPACE":
             typed = typed[:-1]
             draw()
+        #if user presses tab, restart test
+        elif inp.name == "KEY_TAB":
+            typed = ''
+            testWords = createTest()
+            charCount = len(testWords)
+            correct = 0
+            first = True
+            draw()
+        #if user presses anything else, add to typed and check if first character for time
         else:
             typed += inp
             if first:
                 start = time.time()
             first = False
-        
+    else:
+        #display results
+        END = time.time()
+        elapsed = END - start
+        WPS = testLength / elapsed
+        WPM = WPS * 60
+        print(term.home + term.clear)
+        accuracy = (min(correct,charCount) / charCount)*100
+        print("Missed characters: ",max(charCount - correct,0))
+        print("Correct characters: ", correct)
+        print("Accuracy: ", math.floor(accuracy),"%") 
+        print("Time Elapsed: ", round(elapsed,2),"s")
+        print("WPM: ",math.floor(WPM*accuracy/100))
+        print("Raw WPM: ",math.floor(WPM),"\n\n")
+        print("Press TAB to restart test\n")
+        print("Press anything else to quit")
+        with term.cbreak(), term.hidden_cursor():
+            inp = term.inkey()
+        if inp.name == "KEY_TAB":
+            testActive = True
+            typed = ''
+            testWords = createTest()
+            charCount = len(testWords)
+            correct = 0
+            first = True
+            draw()
+        else:
+            break
 
         
 
-END = time.time()
-elapsed = END - start
-WPS = testLength / elapsed
-WPM = WPS * 60
-print(term.home + term.clear)
-accuracy = (min(correct,charCount) / charCount)*100
-print("Missed characters: ",missed)
-print("Accuracy: ", math.floor(accuracy),"%") 
-print("Time Elapsed: ", round(elapsed,2),"s")
-print("WPM: ",math.floor(WPM*accuracy/100))
-time.sleep(20)
-testActive = False
 
-   #time.sleep(.1)
